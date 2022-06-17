b1f393f4a662488b5044bb0ecc9eef56d8de7c8d
Nicholas Gersbach <54191233+Nick-Gersbach@users.noreply.github.com>
2022-06-17 07:52:42 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const usernameInput = document.getElementById("username-input");
+
+usernameInput.addEventListener("keyup", function (event) {
+  let searchQuery = event.target.value.toLowerCase();
+  let allNamesDOMCollection = document.getElementsByClassName("name");
+  for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+    const currentName =
+      allNamesDOMCollection[counter].textContent.toLocaleLowerCase();
+    if (currentName.includes(searchQuery)) {
+      allNamesDOMCollection[counter].style.display = "block";
+    } else {
+      allNamesDOMCollection[counter].style.display = "none";
+    }
+  }
+});
